556d7d9215d5c57f335b9f2f62e604313bada986
geyslan <geyslan@gmail.com>
2013-03-27 08:55:50 -0300

    3rd

--- 3rd.assignment/egg.asm
+++ 3rd.assignment/egg.asm
@@ -1,44 +1,31 @@
-; Egg - Assembly Language
-; Linux/x86
-;
-; Written in 2013 by Geyslan G. Bem, Hacking bits
+; Egg - Assembly Language - Linux/x86
+; Copyright (C) 2013 Geyslan G. Bem, Hacking bits
 ;
 ;   http://hackingbits.com
 ;   geyslan@gmail.com
 ;
-; This source is licensed under the Creative Commons
-; Attribution-ShareAlike 3.0 Brazil License.
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
 ;
-; To view a copy of this license, visit
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
 ;
-;   http://creativecommons.org/licenses/by-sa/3.0/
-;
-; You are free:
-;
-;    to Share - to copy, distribute and transmit the work
-;    to Remix - to adapt the work
-;    to make commercial use of the work
-;
-; Under the following conditions:
-;   Attribution - You must attribute the work in the manner
-;                 specified by the author or licensor (but
-;                 not in any way that suggests that they
-;                 endorse you or your use of the work).
-;
-;   Share Alike - If you alter, transform, or build upon
-;                 this work, you may distribute the
-;                 resulting work only under the same or
-;                 similar license to this one.
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-; egg
+;   egg
 ;
 ;
-; # nasm -f elf32 egg.asm
-; # ld -m elf_i386 egg.o -o egg
+;   # nasm -f elf32 egg.asm
+;   # ld -m elf_i386 egg.o -o egg
 ;
-; Testing
-; # ./egg
+;   Testing
+;   # ./egg
 
 
 global _start
